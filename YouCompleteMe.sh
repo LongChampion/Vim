69fed74a70d9d7b55c76b97e225d3df9c82270a9
LongChampion <ThePalazin@Gmail.com>
2020-07-16 20:21:30 +0700

    Add YouCompleteMe.sh

---
+++ YouCompleteMe.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ ! -d $HOME/.vim ]]; then
+    echo "Where is your $HOME/.vim ?"
+    exit 1
+fi
+
+cd $HOME/.vim/
+
+if [[ ! -d ./pack/tool/start/YouCompleteMe ]]; then
+    echo "Cloning YouCompleteMe"
+    git clone --depth 1 https://github.com/ycm-core/YouCompleteMe ./pack/tool/start/
+else
+    echo "Found YouCompleteMe!"
+fi
+
+cd ./pack/tool/start/YouCompleteMe
+git pull --depth 1
+git submodule update --depth 1 --init --recursive
+
+if [[ $1 == "build" ]]; then
+    ./install.py --clang-completer
+fi
